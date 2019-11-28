b7b0c7ab5b0e9c2145537cb2cb20d95a6bcf8836
kusfabiann <kuz.fabian@gmail.com>
2019-11-28 23:46:06 +0700

    first commit

---
+++ app/src/main/java/com/kus/mapbox/MainActivity.java
@@ -0,0 +1,100 @@
+package com.kus.mapbox;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, "pk.eyJ1Ijoia3VzZmFiaWFubiIsImEiOiJjazM5aXgycDUwM2VoM2lsZDEzNXo1bnByIn0.gBdclxOnmxhUmEWjuUY0wQ");
+        setContentView(R.layout.activity_main);
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+
+                        // Map is set up and the style has loaded. Now you can add data or make other map adjustments.
+
+
+                    }
+                });
+
+                marker(-5.422327, 105.274628, mapboxMap);
+                marker(-5.425937, 105.269717, mapboxMap);
+                marker(-5.375244, 105.246550, mapboxMap);
+                marker(-5.382016, 105.259615, mapboxMap);
+                marker(-5.419785, 105.251958, mapboxMap);
+            }
+        });
+
+    }
+
+    public void marker(double lat,double lon, MapboxMap mapboxMap){
+        MarkerOptions options = new MarkerOptions();
+        options.setTitle("Posisi Sekarang");
+        options.position(new LatLng(lat, lon));
+        mapboxMap.addMarker(options);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+
+}
